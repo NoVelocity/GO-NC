06e250950a51234fbcaf660934bab4ce847b864f
Robo_Start <138618375+RoboStartCoder@users.noreply.github.com>
2026-09-21 20:00:58 +0300

    Minor fixes 2.5

--- lib/espLib/ChinaNetworkCardController.h
+++ lib/espLib/ChinaNetworkCardController.h
@@ -2,8 +2,8 @@
 // Created by Student on 08.08.2026.
 //
 
-#ifndef ESPCHINAWIFINUMBER1_CHINANETWORKCARDCONTROLLER_H
-#define ESPCHINAWIFINUMBER1_CHINANETWORKCARDCONTROLLER_H
+#ifndef CHINANETWORKCARDCONTROLLER_H
+#define CHINANETWORKCARDCONTROLLER_H
 
 #include "ESP8266WiFi.h"
 #include "WebSocketClient.h"
@@ -11,28 +11,32 @@
 #include "JsonUtility.h"
 #include <Arduino.h>
 
-class ChinaNetworkCardController
-{
-
-uint8_t connectionState;
-static constexpr int MAX_CONNECTION_TIME = 10000;
-unsigned long connectionStartTime;
-String globalIp;
-uint16_t globalPort;
+class ChinaNetworkCardController {
+    static constexpr int MAX_CONNECTION_TIME = 10000;
+    uint8_t connectionState;
+    unsigned long connectionStartTime;
+    String globalIp;
+    uint16_t globalPort;
 
 public:
     ChinaNetworkCardController() = default;
+
     void setup();
+
     void loop();
 
     void reset();
-    void listenedSerials();
-    void parseJson(const String& string);
 
-    static void sendMessage(const String& message);
+    void listenedSerials();
+
+    void parseJson(const String &string);
+
+    static void sendMessage(const String &message);
+
     static String getReceivedMessage();
-    void connectTo(const String& ssid, const String& password, const String& ip, const String& path, const int& port);
+
+    void connectTo(const String &ssid, const String &password, const String &ip, const String &path, const int &port);
 };
 
 
-#endif //ESPCHINAWIFINUMBER1_CHINANETWORKCARDCONTROLLER_H
+#endif //CHINANETWORKCARDCONTROLLER_H
